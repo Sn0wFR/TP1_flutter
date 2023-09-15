cf799b429e908a19e863cf775ae2cc4d95402b23
Mathieu <ferreira.mathieu.pro@gmail.com>
2023-09-15 20:07:44 +0200

    convert ChoiceItem to StatefulWidget and handle color change

--- lib/choice_item.dart
+++ lib/choice_item.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 
-class ChoiceItem extends StatelessWidget {
+class ChoiceItem extends StatefulWidget {
   const ChoiceItem({super.key, required this.text});
 
   final String text;
 
   @override
+  State<ChoiceItem> createState() => _ChoiceItemState();
+}
+
+class _ChoiceItemState extends State<ChoiceItem> {
+  
+  Color _color = Colors.grey.withOpacity(0.30);
+  
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        color: Colors.grey.withOpacity(0.30),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Text(text),
+    return GestureDetector(
+      onTap: _onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: _color,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text(widget.text),
+        ),
       ),
     );
+  }
+
+  void _onTap(){
+    setState(() {
+      _color = _color == Colors.orange ? Colors.grey.withOpacity(0.30) : Colors.orange;
+    });
   }
 }
